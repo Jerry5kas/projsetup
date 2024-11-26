a64178f474bbb9fc78213e0cde7666d885f40904
Jerry5kas <122659642+Jerry5kas@users.noreply.github.com>
2024-11-26 22:45:49 +0530

    #7 working on product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
@@ -25,5 +26,22 @@ class ProductController extends Controller
            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
            'is_active' => 'sometimes'
        ]);
+       if ($request->has('image')) {
+            $file = $request->file('image');
+            $extension = $file->getClientOriginalExtension();
+            $fileName = time() . '.' . $extension;
+            $path = 'uploads/product/';
+            $file->move($path , $fileName);
+        }
+        Category::create([
+            'name' => $request->name,
+            'body' => $request->body,
+            'price' => $request->price,
+            'brand' => $request->brand,
+            'category' => $request->category,
+            'image' => $path.$fileName,
+            'is_active' => $request->is_active == true ? 1 : 0,
+        ]);
+        return redirect()->route('product.index')->with('message', 'Product Created Successfully');
    }
 }
--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -1,19 +1,39 @@
 <x-layouts.guest>
     <div class="bg-gray-600/20 h-screen flex justify-center items-center font-lex">
-        <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data" class="w-3/12 mx-auto p-5 flex flex-col gap-y-5 bg-white rounded-md">
+        <form action="{{ route('product.store') }}" method="POST" enctype="multipart/form-data"
+            class="w-3/12 mx-auto p-5 flex flex-col gap-y-5 bg-white rounded-md">
             @csrf
-            <input type="text" name="name" placeholder="Product Name" class="border p-2 rounded text-xs">
-            <textarea name="body" placeholder="Product Description"  class="border p-2 rounded text-xs"></textarea>
-            <input type="text" name="brand" placeholder="Brand Name" class="border p-2 rounded text-xs">
-             <input type="number" id="quantity" name="price" min="1" placeholder="Price Name" max="5" class="border p-2 rounded text-xs">
-            <input type="range" name="price" placeholder="Category Name" class="border p-2 rounded text-xs">
+            <input type="text" name="name" placeholder="Product Name" value="{{ old('name') }}"
+                class="border p-2 rounded text-xs">
+            @error('name')
+                <span class="text-red-600 text-xs">{{ $message }}</span>
+            @enderror
+            <textarea name="body" placeholder="Product Description" class="border p-2 rounded text-xs">{{ old('body') }}</textarea>
+            @error('body')
+                <span class="text-red-600 text-xs">{{ $message }}</span>
+            @enderror
+            <input type="text" name="brand" placeholder="Brand Name" value="{{ old('brand') }}"
+                class="border p-2 rounded text-xs">
+            @error('brand')
+                <span class="text-red-600 text-xs">{{ $message }}</span>
+            @enderror
+            <input type="text" name="price" placeholder="Price Name" value="{{ old('price') }}"
+                class="border p-2 rounded text-xs">
+            @error('price')
+                <span class="text-red-600 text-xs">{{ $message }}</span>
+            @enderror
+            <input type="text" name="category" placeholder="Category Name" value="{{ old('category') }}"
+                class="border p-2 rounded text-xs">
+            @error('category')
+                <span class="text-red-600 text-xs">{{ $message }}</span>
+            @enderror
             <div class="flex-col flex gap-y-3 text-xs text-gray-700 font-semibold border rounded-md p-2">
                 <label for="">Upload Product</label>
-            <input type="file" name="image">
+                <input type="file" name="image">
             </div>
             <div class="flex gap-x-3 text-xs text-gray-700">
                 <label for="">Status</label>
-            <input type="checkbox" name="is_active" checked>
+                <input type="checkbox" name="is_active" {{ old('is_active') ? 'checked' : '' }}>
             </div>
             <button type="submit" class="px-6 py-2 bg-blue-600 text-white rounded-md">Save</button>
         </form>
